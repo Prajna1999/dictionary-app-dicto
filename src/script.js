33e22d0563ecd4c0e9deab9070fe188dbb8acbc6
Prajna1999 <gituprajna20@gmail.com>
2022-04-28 20:36:41 +0530

    Feat: cross mark functionality

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 const wrapper=document.querySelector(".container");
-
+const synonyms=document.querySelector(".synonyms .list");
 const searchInput=wrapper.querySelector("input");
 const infoText=wrapper.querySelector(".info-text");
 
@@ -9,13 +9,14 @@ searchInput.addEventListener("keyup",(e)=>{
 })
 
 function fetchApi(word){
+    wrapper.classList.remove("active");
     infoText.style.color="#000";
     infoText.innerHTML=`getting data for the <span>"${word}"</span>...`
 
     const url=`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
 
     fetch(url).then((response)=>{
-        if(!response.ok){
+        if(response.status===404){
             throw new Error("Error: It is us!")
         }
         return response.json();
@@ -24,16 +25,45 @@ function fetchApi(word){
         data(result);
     }).catch((e)=>{
         console.log(e.message)
-        infoText.innerHTML=`Sorry pal, we could not find the definition for the <span>"${word}"</span> you're looking for.`;
+        if(wrapper.classList.contains("active")){
+            wrapper.classList.remove("active");
+            
+            infoText.innerHTML=`Sorry pal, we could not find the definition for the <span>"${word}"</span> you're looking for.`;
+        }else{
+
+            infoText.innerHTML=`Sorry pal, we could not find the definition for the <span>"${word}"</span> you're looking for.`;
+        }
+        
     });
 }
 
 function data(result){
     wrapper.classList.add("active");
-
+    console.log(result)
     const definitions=result[0].meanings[0].definitions[0];
     const phonetics=`${result[0].meanings[0].partOfSpeech} /${result[0].phonetic}/`;
     document.querySelector(".word p").innerText=result[0].word;
     document.querySelector(".word span").innerText=phonetics;
     document.querySelector(".meaning span").innerText=definitions.definition;
+
+
+    document.querySelector(".example span").innerText=definitions.example;
+
+    if(result[0].meanings[0].synonyms[0]==undefined){
+        synonyms.parentElement.style.display="none";
+    }else{
+        synonyms.innerHTML=""
+        synonyms.parentElement.style.display="block"
+        for(let i=0; i<5;i++){
+            const tag=`<span>${result[0].meanings[0].synonyms[i]},</span>`;
+            synonyms.insertAdjacentHTML("beforeend", tag);
+        }
+    }
+
+    
 }
+document.querySelector(".searchbar span").addEventListener("click", ()=>{
+    searchInput.value="";
+    searchInput.focus();
+    wrapper.classList.remove("active");
+})
